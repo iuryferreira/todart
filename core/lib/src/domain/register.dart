7025a966f65edc57e84264c72a44fe12c648878b
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-01-26 03:19:58 -0300

    ✨ register auth

--- core/lib/src/domain/register.dart
+++ core/lib/src/domain/register.dart
@@ -3,13 +3,17 @@ import 'package:ioco/ioco.dart';
 import '../shared/usecases/todo.dart';
 import '../shared/usecases/user.dart';
 import '../shared/usecases/task.dart';
+import '../shared/usecases/auth.dart';
 
 import 'usecases/user.dart';
 import 'usecases/todo.dart';
 import 'usecases/task.dart';
+import 'usecases/auth.dart';
 
 void registerDomain(Ioco container) {
   container.bind(ITodoUseCase, TodoUseCase);
   container.bind(IUserUseCase, UserUseCase);
   container.bind(ITaskUseCase, TaskUseCase);
+  container.bind(ITaskUseCase, TaskUseCase);
+  container.bind(IAuthUseCase, AuthUseCase);
 }
